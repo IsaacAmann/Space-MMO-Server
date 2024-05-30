3e7b9b049b824614ad9a0263519f944e7b6a46b6
IsaacAmann <isaacmnn@gmail.com>
2024-05-30 08:39:19 -0500

    Moved files

---
+++ backend/src/main/java/com/SpaceMMO/SpaceMmoApplication.java
@@ -0,0 +1,13 @@
+package com.SpaceMMO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceMmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceMmoApplication.class, args);
+	}
+
+}
