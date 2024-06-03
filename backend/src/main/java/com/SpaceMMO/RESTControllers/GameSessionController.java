814431850dfbd714e3e7b92fcff1fa1620869e6d
IsaacAmann <isaacmnn@gmail.com>
2024-06-03 16:18:48 -0500

    Started game session stuff

---
+++ backend/src/main/java/com/SpaceMMO/RESTControllers/GameSessionController.java
@@ -0,0 +1,46 @@
+package com.SpaceMMO.RESTControllers;
+
+import com.SpaceMMO.Services.AuthorizationService;
+import com.SpaceMMO.UserManagement.UserAccount;
+import com.SpaceMMO.UserManagement.UserAccountRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GameSessionController
+{
+    @Autowired
+    AuthorizationService authorizationService;
+    @Autowired
+    UserAccountRepository userAccountRepository;
+
+    @PostMapping("/api/authTest")
+    public Map<String, Object> authTest(@RequestBody Map<String, Object> payload, HttpServletRequest request)
+    {
+        HashMap<String, Object> output = new HashMap<String, Object>();
+
+        UserAccount account = authorizationService.getAccountFromToken(request);
+
+        if(account.inGame == false)
+        {
+            //Generate new session token
+            account.generateGameToken();
+            //Save account
+            userAccountRepository.save(account);
+            //Pass token to user
+            output.put("gameSessionToken", account.getEncodedGameToken());
+        }
+        else
+        {
+            //Return error
+            output.put("error", "User signed in on another session");
+        }
+
+        return output;
+    }
+}
+
--- backend/src/main/java/com/SpaceMMO/UserManagement/UserAccount.java
+++ backend/src/main/java/com/SpaceMMO/UserManagement/UserAccount.java
@@ -7,6 +7,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import org.springframework.security.core.parameters.P;
 
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Base64;
+
 @Entity
 public class UserAccount
 {
@@ -24,16 +28,53 @@ public class UserAccount
 
     public UserRole userRole;
 
+    //Indicates whether or not player is currently connected to game session
+    public boolean inGame;
+
+    //Current game session token used to establish websocket connection
+    private byte[] gameSessionToken;
+
     public UserAccount(String username, UserRole userRole)
     {
         this.username = username;
         this.userRole = userRole;
+        inGame = false;
     }
 
     public UserAccount()
     {
         userRole = UserRole.USER;
         username = null;
+        inGame = false;
     }
 
+    //Return encoded session token
+    public String getEncodedGameToken()
+    {
+        Base64.Encoder encoder = Base64.getUrlEncoder();
+
+        return (encoder.encodeToString(gameSessionToken));
+    }
+
+    //Check if a given Game session Token is valid
+    public boolean isValidGameToken(String token)
+    {
+        boolean output = false;
+        //Decode token
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+        byte[] tokenBytes = decoder.decode(token);
+        //Compare passed token to stored token
+        if(Arrays.equals(tokenBytes, this.gameSessionToken))
+        {
+            output = true;
+        }
+
+        return output;
+    }
+
+    public void generateGameToken()
+    {
+        SecureRandom tokenGen = new SecureRandom();
+        tokenGen.nextBytes(this.gameSessionToken);
+    }
 }
