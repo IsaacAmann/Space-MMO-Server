d2c8dec7cb3c35805348594f9313cad1de946d91
IsaacAmann <isaacmnn@gmail.com>
2024-06-03 15:05:49 -0500

    Added rate limit service

--- backend/src/main/java/com/SpaceMMO/SpaceMmoApplication.java
+++ backend/src/main/java/com/SpaceMMO/SpaceMmoApplication.java
@@ -1,20 +1,19 @@
 package com.SpaceMMO;
 
+import com.SpaceMMO.Services.RateLimitService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpaceMmoApplication {
-
-	/*
-	public static void main(String[] args) {
-		SpringApplication.run(SpaceMmoApplication.class, args);
-	}
-	*/
+	@Autowired
+	RateLimitService rateLimitService;
 
 	public static void main(String[] args)
 	{
@@ -32,7 +31,13 @@ public class SpaceMmoApplication {
 		{
 			registry.addMapping("/**");
 		}
+		@Override
+		public void addInterceptors(InterceptorRegistry registry)
+		{
+			RateLimitService.RateLimitInterceptor interceptor = rateLimitService.getInterceptor();
 
+			registry.addInterceptor(interceptor).addPathPatterns("/*");
+		}
 
 	}
 }
